7e5ffa20dade052cfbd0358bec2173a8aef9f432
John Stovin <john.d.stovin@gmail.com>
2014-04-24 08:48:16 +0100

    Simplified redistribution

--- Mancala.Tests/Mancala.Tests.fs
+++ Mancala.Tests/Mancala.Tests.fs
@@ -5,57 +5,78 @@ open Mancala
 
 [<Fact>]
 let BoardHasTwoSides() =
-    let state = new BoardState(0)
+    let state = CreateBoard(0)
     Assert.Equal(2, state.Sides.Length)
 
 [<Fact>]
 let SideOneHasSevenPits() =
-    let state = new BoardState(0)
+    let state = CreateBoard(0)
     Assert.Equal(7, state.Sides.[0].Length)
 
 [<Fact>]
 let SideTwoHasSevenPits() =
-    let state = new BoardState(0)
+    let state = CreateBoard(0)
     Assert.Equal(7, state.Sides.[1].Length)
 
 [<Fact>]
 let BoardCanBeInitialisedWith3StonesPerPit() =
-    let state = new BoardState(3)
+    let state = CreateBoard(3)
     Assert.Equal<int[]> ([|0;3;3;3;3;3;3|], state.Sides.[0])
     Assert.Equal<int[]> ([|0;3;3;3;3;3;3|], state.Sides.[1])
 
 [<Fact>]
-let PlayerCannotPlayInvalidPayer() =
-    let state = new BoardState(3)
+let PlayerCannotPlayInvalidPlayer() =
+    let state = CreateBoard(3)
     let allowed = CanPlay state 2 0 
     Assert.False(allowed)
 
 [<Fact>]
 let PlayerCannotPlayInvalidPit() =
-    let state = new BoardState(3)
+    let state = CreateBoard(3)
     let allowed = CanPlay state 1 7 
     Assert.False(allowed)
 
 [<Fact>]
 let PlayerCannotPlayEmptyPit() =
-    let state = new BoardState(0)
+    let state = CreateBoard(0)
     let allowed = CanPlay state 1 5 
     Assert.False(allowed)
 
 [<Fact>]
 let PlayerCanPlayEmptyPit() =
-    let state = new BoardState(3)
+    let state = CreateBoard(3)
     let allowed = CanPlay state 1 5 
     Assert.True(allowed)
 
 [<Fact>]
 let BoardWithNonEmptyPitsIsNotFinished() =
-    let state = new BoardState(3)
+    let state = CreateBoard(3)
     let finished = IsFinished state
     Assert.False(finished)
 
 [<Fact>]
 let BoardWithEmptyPitsOnOneSideIsFinished() =
-    let state = new BoardState(0)
+    let state = CreateBoard(0)
     let finished = IsFinished state
     Assert.True(finished)
+
+[<Fact>]
+let PlayRedistributesStonesOnPlayersSide() =
+    let state = CreateBoard(3)
+    let newState = Play state 0 6
+    Assert.Equal<int[]> ([|0;3;3;4;4;4;0|], newState.Sides.[0])
+    Assert.Equal<int[]> ([|0;3;3;3;3;3;3|], newState.Sides.[1])
+
+[<Fact>]
+let PlayRedistributesStonesIntoScoring() =
+    let state = CreateBoard(3)
+    let newState = Play state 0 3
+    Assert.Equal<int[]> ([|1;4;4;0;3;3;3|], newState.Sides.[0])
+    Assert.Equal<int[]> ([|0;3;3;3;3;3;3|], newState.Sides.[1])
+
+//[<Fact>]
+//let PlayRedistributesStonesIntoOpponentsSide() =
+//    let state = CreateBoard(3)
+//    let newState = Play state 0 1
+//    Assert.Equal<int[]> ([|1;0;3;3;3;3;3|], newState.Sides.[0])
+//    Assert.Equal<int[]> ([|0;3;3;3;3;4;4|], newState.Sides.[1])
--- Mancala/Mancala.fs
+++ Mancala/Mancala.fs
@@ -1,23 +1,47 @@
 ﻿module Mancala
 
-type BoardState(stonesPerPit) = 
-    let CreateSide(stonesPerPit : int) =
-        [| 0; stonesPerPit; stonesPerPit; stonesPerPit; stonesPerPit; stonesPerPit; stonesPerPit |]
+type BoardState(side1, side2) = 
+    member x.Sides = [| side1; side2 |]
 
-    member x.Sides = [| CreateSide(stonesPerPit); CreateSide(stonesPerPit) |]
+let CreateBoard(stonesPerPit : int) =
+    let CreateSide(stonesPerPit : int) = 
+        Array.init 7 (fun i -> if i = 0 then 0 else stonesPerPit)
+
+    new BoardState(CreateSide(stonesPerPit), CreateSide(stonesPerPit))
 
 let CanPlay (state : BoardState) player pit =
-    match player with
-    | 0 | 1 -> 
-        match pit with 
-        | 1 | 2 | 3 | 4 | 5 | 6 ->
-            state.Sides.[player].[pit] > 0
-        | _ -> false
-    | _ -> false
+        if (player < state.Sides.Length && pit < state.Sides.[0].Length)
+        then state.Sides.[player].[pit] > 0
+        else false 
 
 let IsFinished(state : BoardState) = 
     let finished (side : int array) = 
-        side.[1..6] = [|0;0;0;0;0;0|]
-
+        side.[1..6] |> Array.sum = 0
     finished state.Sides.[0] || finished state.Sides.[1]
+
+let IncrementPit (side : int array) pit =
+    Array.append side.[0..(pit-1)] (Array.append [|(side.[pit] + 1)|] side.[(pit+1)..])
+
+let EmptyPit (side : int array) pit =
+    Array.append side.[0..(pit-1)] (Array.append [|0|] side.[(pit+1)..])
+    
+let rec Distribute side pit count =
+    match count with
+    | 0 -> side
+    | 1 -> IncrementPit side pit
+    | _ -> Distribute (IncrementPit side pit) (pit-1) (count-1)
+
+let Redistribute (side : int array) pit =
+    let count = side.[pit]
+    Distribute (EmptyPit side pit) (pit-1) count
+
+
+let Play (state : BoardState) player pit =
+    match CanPlay state player pit with
+    | false -> state
+    | true -> 
+        match player with
+        | 0 -> new BoardState ((Redistribute state.Sides.[0] pit), state.Sides.[1])
+        | 1 -> new BoardState (state.Sides.[0], (Redistribute state.Sides.[1] pit))
+        | _ -> state
     
